317d35a965b49776ba706fc5fae7ae83fcb9f931
jucs7 <jucs0709@gmail.com>
2025-10-21 15:12:45 -0500

    feat(User): add role column to user table & model

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -44,6 +45,19 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'role' => 'string',
         ];
+    }
+
+    /**
+     * Verifies if user has role.
+     * 
+     * @param string $role User role
+     * 
+     * @return bool
+     */
+    public function hasRole(string $role): bool
+    {
+        return $this->role === $role;
     }
 }
