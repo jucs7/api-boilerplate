4fd2f65ff2d61f90b1a24a1a649e2e607350449d
jucs7 <jucs0709@gmail.com>
2025-10-22 17:15:08 -0500

    feat(Models): create Product model

---
+++ app/Models/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductModelFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'description',
+        'price',
+        'stock',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+}
