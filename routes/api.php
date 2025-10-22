01d3d27bbabdf58a9db11e296ec56aebab68d4f4
jucs7 <jucs0709@gmail.com>
2025-10-21 22:52:34 -0500

    feat: create Auth routes & controller

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::post('/logout', [AuthController::class, 'logout']);
     
     Route::apiResource('categories', CategoryController::class);
 
