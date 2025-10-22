546490bcf5a545cf8b723f3564e7fc50b616af83
jucs7 <jucs0709@gmail.com>
2025-10-21 22:07:36 -0500

    feat: add User repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Repositories\Contracts\CategoryRepositoryInterface;
+use App\Repositories\Contracts\UserRepositoryInterface;
 use App\Repositories\Eloquent\CategoryRepository;
+use App\Repositories\Eloquent\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,8 +16,13 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(
+            UserRepositoryInterface::class,
+            UserRepository::class
+        );
+
+        $this->app->bind(
             CategoryRepositoryInterface::class,
-            CategoryRepository::class,
+            CategoryRepository::class
         );
     }
 
---
+++ app/Repositories/Eloquent/UserRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Repositories\Contracts\UserRepositoryInterface;
+use App\Models\User;
+
+class UserRepository implements UserRepositoryInterface
+{
+    public function findByEmail(string $email): ?User
+    {
+        return User::where('email', $email)->first();
+    }
+
+    public function create(array $data): User
+    {
+        $data['password'] = bcrypt($data['password']);
+        return User::create($data);
+    }
+}
